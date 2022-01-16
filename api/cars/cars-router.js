9437be294ffba2c57abde4781513a9f940d91127
VABIII <tbengtson82@gmail.com>
2022-01-16 16:34:39 -0600

    First cars ep built. .then() and try/catch

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,10 +1,26 @@
 // const express = require('express')
 // const router = express.Router()
-
 const router = require('express').Router()
 
+const Cars = require('./cars-model')
+
+// router.get('/', async (req, res, next) => {
+//     Cars.getAll()
+//         .then(cars => {
+//             res.json(cars)
+//         })
+//         .catch(next)
+// })
+
 router.get('/', async (req, res, next) => {
-    res.json('getting all cars')
+    try {
+        const cars = await Cars.getAll()
+        res.json(cars)
+    }
+    catch(err) {
+        next(err)
+    }
+
 })
 
 router.get('/:id', async (req, res, next) => {
