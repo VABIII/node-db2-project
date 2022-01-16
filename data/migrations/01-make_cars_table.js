4fd8f3421d32c3dfb98abb36f631ce997b5dad36
VABIII <tbengtson82@gmail.com>
2022-01-16 15:43:42 -0600

    migration up function fleshed-out

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,5 +1,14 @@
 exports.up = function (knex) {
-  // DO YOUR MAGIC
+    return knex.schema.createTable('cars', tbl => {
+        tbl.increments()
+
+        tbl.string('vin', 17).unique().notNullable()
+        tbl.string('make', 100).notNullable()
+        tbl.string('model', 100).notNullable()
+        tbl.integer('mileage').unsigned().notNullable() //unsigned() prevents mileage from being a negative #
+        tbl.string('title', 100).defaultTo('clean')
+        tbl.string('transmission', 100).defaultTo('automatic')
+    })
 };
 
 exports.down = function (knex) {
