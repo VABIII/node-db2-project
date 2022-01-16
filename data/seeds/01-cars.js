183887b109649b84edd4857d0dbec05dd62e3e7a
VABIII <tbengtson82@gmail.com>
2022-01-16 15:20:07 -0600

    seed dummy data created

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,95 @@
-// STRETCH
+const cars = [
+    {
+        vin: '123456789012345678',
+        make: 'toyota',
+        model: 'tundra',
+        mileage: 123445,
+        title: 'clean',
+        transmission: 'automatic'
+
+    },
+    {
+        vin: '123456789012345678',
+        make: 'honda',
+        model: 'accord',
+        mileage: 123445,
+        title: 'clean',
+        transmission: 'manual'
+
+    },
+    {
+        vin: '123456789012345678',
+        make: 'toyota',
+        model: 'supra',
+        mileage: 123445,
+        title: 'salvage',
+        transmission: 'manual'
+
+    },
+    {
+        vin: '123456789012345678',
+        make: 'pontiac',
+        model: 'firebird',
+        mileage: 123445,
+        title: 'clean',
+        transmission: 'manual'
+
+    }
+]
+
+exports.seed = async function(knex) {
+    await knex('cars').truncate()
+    await knex('cars').insert(cars)
+
+}
+
+// exports.seed = function(knex) { // This is referred to as dependency injection
+//     return knex('cars')
+//         .truncate().then(() => {
+//             return knex('cars').insert(cars)
+//         })
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
